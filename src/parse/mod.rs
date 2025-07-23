4ba68de4153d3c868447b9da3405464d7bba484a
Hannes <github@h-h.win>
2025-07-23 20:53:05 +0200

    get fields as well

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,5 +1,5 @@
 use crate::types::{Class, Field};
-use tree_sitter::{InputEdit, Language, Node, Parser, Point, Tree, TreeCursor};
+use tree_sitter::{Parser, Tree};
 use tree_sitter_dart::language;
 
 pub fn get_tree(code: &str) -> Result<Tree, String> {
@@ -9,31 +9,65 @@ pub fn get_tree(code: &str) -> Result<Tree, String> {
     Ok(tree)
 }
 
-pub fn parse(code: &str, magic_token: &str) -> Result<Vec<Class>, String> {
+pub fn parse<'a>(code: &'a str, magic_token: &'a str) -> Result<Vec<Class<'a>>, String> {
     let tree = get_tree(code)?;
     let root_node = tree.root_node();
     let mut cursor = root_node.walk();
     
     // go into program
     cursor.goto_first_child();
+
+    // now get the class definitions
     let mut classes_to_parse = Vec::new();
     let mut prev_node = root_node;
     loop {
         if cursor.node().kind() == "class_definition" {
             if prev_node.kind() != "comment" {continue};
             if !prev_node.utf8_text(&code.as_bytes()).unwrap().contains(magic_token) {continue};
-            let class_name = cursor.node().child(1).unwrap().utf8_text(&code.as_bytes()).unwrap();
-            let class_body = cursor.node().child(2).unwrap();
-            classes_to_parse.push((class_name, class_body.walk()));
+            let class_name = cursor.node().child_by_field_name("name").unwrap().utf8_text(&code.as_bytes()).unwrap();
+            let class_body = cursor.node().child_by_field_name("body").unwrap();
+            classes_to_parse.push((class_name, class_body));
         }
         prev_node = cursor.node();
         if !cursor.goto_next_sibling() {
             break;
         }
     }
-    // now get the class definitions
-    println!("{:?}",classes_to_parse.iter().map(|(name, body)| name).collect::<Vec<_>>());
-    Ok(Vec::new())
+
+    // now get the fields in these classes
+    let mut classes_with_fields = Vec::new();
+    for (class_name, class_body) in classes_to_parse {
+        let mut fields = Vec::new();
+        let mut cursor = class_body.walk();
+        let field_declarations = class_body.named_children(&mut cursor).filter(|node|node.kind() == "declaration").collect::<Vec<_>>();
+        for field_declaration in field_declarations {
+            let mut field_name = None;
+            let mut field_type = None;
+            let mut field_is_nullable = false;
+            for child in field_declaration.named_children(&mut cursor) {
+                if child.kind() == "type_identifier" {
+                    field_type = Some(child.utf8_text(&code.as_bytes()).unwrap());
+                } else if child.kind() == "nullable_type" {
+                    field_is_nullable = true;
+                } else if child.kind() == "initialized_identifier_list" {
+                    field_name = child.named_child(0).unwrap().utf8_text(&code.as_bytes()).ok();
+                }
+            }
+            if let (Some(name_str), Some(type_str)) = (field_name, field_type) {
+                fields.push(Field {
+                    name_str,
+                    type_str,
+                    is_nullable: field_is_nullable,
+                });
+            };
+        }
+        classes_with_fields.push(Class {
+            name_str: class_name,
+            fields,
+        });
+    }
+
+    Ok(classes_with_fields)
 }   
 
 #[cfg(test)]
@@ -51,6 +85,7 @@ mod tests {
     #[test]
     fn test_parse_in1() {
         let code = include_str!("../../test/in1.dart");
-        let fields = parse(code, "+mk:").unwrap();
+        let classes = parse(code, "+mk:").unwrap();
+        println!("{:?}", classes);
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,10 +1,12 @@
-pub struct Field {
-    /// includes ? at the end if null-able
-    typeStr: String,
-    nameStr: String,
+#[derive(Debug)]
+pub struct Field<'a> {
+    pub type_str: &'a str,
+    pub name_str: &'a str,
+    pub is_nullable: bool,
 }
 
-pub struct Class {
-    name: String,
-    fields: Vec<Field>,
+#[derive(Debug)]
+pub struct Class<'a> {
+    pub name_str: &'a str,
+    pub fields: Vec<Field<'a>>,
 }
