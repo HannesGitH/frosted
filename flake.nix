637770ef602e0ebc53d33519f72954bc542fb6c9
Hannes <github@h-h.win>
2025-07-23 18:04:19 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "frosted: freezed light";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+      inherit (nixpkgs) lib;
+      systems = lib.systems.flakeExposed;
+      forAllSystems = lib.genAttrs systems;
+      spkgs = system: nixpkgs.legacyPackages.${system}.pkgs;
+    in {
+      packages = forAllSystems (s: with spkgs s; rec {
+        parser = rustPlatform.buildRustPackage {
+          pname = "frosted";
+          version = "0.0.1";
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          nativeBuildInputs = [
+            pkg-config
+          ];
+          buildInputs = [
+            openssl
+          ];
+        };
+        default = parser;
+      });
+
+      devShells = forAllSystems (s: with spkgs s; {
+        default = mkShell {
+          RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+          buildInputs = [
+            cargo
+            rustc
+          ];
+        };
+      });
+  };
+}
